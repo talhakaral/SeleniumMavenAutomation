aabc295c66d3b3612cba72c9de456ce75af4ad6e
user <user@users-MacBook-Air-2.local>
2018-06-13 17:27:09 -0500

    add pomxml and src

---
+++ src/test/java/com/dice/DiceJobSearch.java
@@ -0,0 +1,59 @@
+package com.dice;
+
+import java.util.concurrent.TimeUnit;
+
+import javax.management.RuntimeErrorException;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class DiceJobSearch {
+	public static void main(String[] args) {
+
+		WebDriverManager.chromedriver().setup();
+		
+		WebDriver driver = new ChromeDriver();
+		
+		driver.manage().window().fullscreen();
+	
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		
+		String url = "https://dice.com";
+		driver.get(url);
+		
+		String actualTitle = driver.getTitle();
+		String expectedTitle = "Job Search for Technology Professionals | Dice.com";
+		
+		if(actualTitle.equals(expectedTitle)) {
+			System.out.println("Step pass . Dice homepage successfully loaded");
+		}else {
+			System.out.println("Step fail . Dice homepage did not load successfully");
+			throw new RuntimeException("Step fail . Dice homepage did not load successfully");
+		}
+		
+		String keyword = "java developer";
+		driver.findElement(By.id("search-field-keyword")).clear();
+		driver.findElement(By.id("search-field-keyword")).sendKeys(keyword);
+		
+		String location = "22102";
+		driver.findElement(By.id("search-field-location")).clear();
+		driver.findElement(By.id("search-field-location")).sendKeys(location);
+		
+		driver.findElement(By.id("findTechJobs")).click();
+		
+		String count = driver.findElement(By.id("posiCountId")).getText();
+		System.out.println(count);
+		
+		int countResult = Integer.parseInt(count.replace(",", ""));
+		
+		if(countResult >0) {
+			System.out.println("Keyword : "+keyword + " search returned "+countResult + " result in "+location );
+		}else {
+			System.out.println("Step fail = Keyword : "+keyword + " search returned "+countResult + " result in "+location);
+		}
+		driver.close();
+	}
+}
